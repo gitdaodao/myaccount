408d6643284bac250857b6fe91944c1c9fba0489
gitdaodao <2655750198@qq.com>
2018-07-11 10:09:50 +0800

    用户注册的表单和模板定义

--- useraccount/account/forms.py
+++ useraccount/account/forms.py
@@ -1,6 +1,24 @@
 from django import forms 
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     """用户登录"""
     username=forms.CharField(label='用户名',max_length=15)
     password=forms.CharField(label='密码',widget=forms.PasswordInput)
+
+
+class UserRegisterForm(forms.ModelForm):
+    """依据已有User类模型 造自己需要的用户注册表单"""
+    password=forms.CharField(label='密码',max_length=21,widget=forms.PasswordInput)
+    re_password=forms.CharField(label='重复密码',widget=forms.PasswordInput)
+
+    class Meta:
+        model=User
+        fileds=('username','email','first_name')
+
+    def cl_re_password(self):
+        data=self.cleaned_data
+        if data['password'] != data['re_password']:
+            raise forms.ValidationError('两次输入的密码不一致')
+        return data['re_password']
+
