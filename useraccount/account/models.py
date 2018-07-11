1af8482bd876cbbbc5c02e0dd35ef0d3105cf1e7
gitdaodao <2655750198@qq.com>
2018-07-11 12:19:35 +0800

    扩展用户Profile模型及后台admin模型管理

--- useraccount/account/models.py
+++ useraccount/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Profile(models.Model):
+    mobile=models.CharField('手机号',max_length=21,blank=True)
+    photo=models.ImageField('图片',upload_to='users/%Y/%m/%d',blank=True)
+    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'用户名:{self.user.username}'
